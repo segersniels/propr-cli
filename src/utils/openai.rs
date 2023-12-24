7ad898d36a4a14be22653b7b490ac94da8861dc9
segersniels <segers.n@hotmail.com>
2023-12-24 23:35:36 +0100

    :recycle: Use SDK for regular chat completion as well

--- src/utils/openai.rs
+++ src/utils/openai.rs
@@ -1,14 +1,12 @@
 use async_openai::{
     error::{ApiError, OpenAIError},
     types::{
-        CreateMessageRequestArgs, CreateRunRequestArgs, CreateThreadRequestArgs, MessageContent,
-        RunStatus,
+        ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs,
+        CreateChatCompletionRequestArgs, CreateMessageRequestArgs, CreateRunRequestArgs,
+        CreateThreadRequestArgs, MessageContent, RunStatus,
     },
     Client,
 };
-use reqwest::Error;
-use serde::Deserialize;
-use std::process;
 use tokio::time::sleep;
 
 pub const ALLOWED_MODELS: [&str; 6] = [
@@ -20,7 +18,7 @@ pub const ALLOWED_MODELS: [&str; 6] = [
     "gpt-4-1106-preview",
 ];
 
-const MAX_TOKEN_LENGTH: i32 = 500;
+const MAX_TOKEN_LENGTH: i32 = 512;
 const FILES_TO_IGNORE: [&str; 11] = [
     "package-lock.json",
     "yarn.lock",
@@ -34,28 +32,6 @@ const FILES_TO_IGNORE: [&str; 11] = [
     "Pipfile.lock",
     "composer.lock",
 ];
-
-#[derive(Deserialize, Debug)]
-struct Message {
-    content: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct Choice {
-    message: Message,
-}
-
-#[derive(Deserialize, Debug)]
-struct ResponseError {
-    message: String,
-    code: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct Response {
-    choices: Option<[Choice; 1]>,
-    error: Option<ResponseError>,
-}
 
 fn split_diff_into_chunks(diff: &str) -> Vec<String> {
     diff.split("diff --git")
@@ -108,7 +84,7 @@ fn generate_user_message(diff: &str, template: &str) -> String {
     )
 }
 
-async fn get_assistant_response(
+async fn get_assistant_completion(
     assistant_id: &str,
     diff: &str,
     template: &str,
@@ -188,84 +164,37 @@ async fn get_assistant_response(
     }
 }
 
-fn create_payload(model: &str, system_message: &str, content: &str) -> serde_json::Value {
-    serde_json::json!({
-        "model": model,
-        "messages": [
-            { "role": "system", "content": system_message },
-            { "role": "user", "content": content }
-        ],
-        "temperature": 0.7,
-        "top_p": 1,
-        "frequency_penalty": 0,
-        "presence_penalty": 0,
-        "max_tokens": MAX_TOKEN_LENGTH,
-    })
-}
+async fn get_completion(
+    model: &str,
+    system_message: &str,
+    content: &str,
+) -> Result<String, OpenAIError> {
+    let client = Client::new();
+    let request = CreateChatCompletionRequestArgs::default()
+        .max_tokens(MAX_TOKEN_LENGTH as u16)
+        .model(model)
+        .messages([
+            ChatCompletionRequestSystemMessageArgs::default()
+                .content(system_message)
+                .build()?
+                .into(),
+            ChatCompletionRequestUserMessageArgs::default()
+                .content(content)
+                .build()?
+                .into(),
+        ])
+        .build()?;
 
-/// Perform a completion request to the OpenAI API
-async fn get_chat_completion(body: &serde_json::Value) -> Result<String, Error> {
-    let api_key = std::env::var("OPENAI_API_KEY").unwrap_or_else(|_| {
-        println!("Error: OPENAI_API_KEY environment variable not set");
-        process::exit(0);
-    });
+    let response = client.chat().create(request).await?;
 
-    let response = reqwest::Client::new()
-        .post("https://api.openai.com/v1/chat/completions")
-        .header(
-            reqwest::header::AUTHORIZATION,
-            format!("Bearer {}", api_key),
-        )
-        .header(reqwest::header::CONTENT_TYPE, "application/json")
-        .body(serde_json::to_string(body).unwrap())
-        .send()
-        .await?;
-
-    let result: Result<String, Error> = response.text().await;
-    match result {
-        Ok(response) => {
-            let data: Response = serde_json::from_str(response.as_str()).unwrap();
-
-            if data.error.is_some() {
-                let error = data.error.unwrap();
-
-                match error.code.as_str() {
-                    "context_length_exceeded" => {
-                        println!("Error: The provided diff is too large. Try using a different model that supports a higher token count or reduce the size of the diff.");
-                        std::process::exit(1);
-                    }
-                    _ => {
-                        println!("Error: {}", error.message);
-                        std::process::exit(1);
-                    }
-                }
-            }
-
-            if let Some(choice) = data.choices {
-                Ok(choice[0].message.content.clone())
-            } else {
-                println!("Error: {}", response.as_str());
-                std::process::exit(1);
-            }
-        }
-        Err(_) => {
-            println!("Error: Could not fetch response from OpenAI");
-            std::process::exit(1);
-        }
-    }
+    Ok(response.choices[0].message.content.clone().unwrap())
 }
 
 /// Generate a concise PR title
-pub async fn generate_title(description: &str) -> Result<String, Error> {
+pub async fn generate_title(description: &str) -> Result<String, OpenAIError> {
     let system_message = "Generate a concise PR title from the provided description prefixed with a suitable gitmoji";
 
-    /*
-    Generate the title using gpt-3.5-turbo since it is the fastest model
-    and we don't want to spend too many tokens on this
-    */
-    let body = create_payload("gpt-3.5-turbo-1106", system_message, description);
-
-    get_chat_completion(&body).await
+    get_completion("gpt-3.5-turbo-1106", system_message, description).await
 }
 
 /// Generate a concise PR description
@@ -274,11 +203,10 @@ pub async fn generate_description(
     diff: &str,
     template: &str,
     model: &str,
-) -> Result<String, Error> {
+) -> Result<String, OpenAIError> {
     let system_message = generate_system_message_for_diff(prompt, template);
-    let body = create_payload(model, &system_message, &prepare_diff(diff));
 
-    get_chat_completion(&body).await
+    get_completion(model, &system_message, &prepare_diff(diff)).await
 }
 
 /// Generate a PR description using the user specified assistant
@@ -287,5 +215,5 @@ pub async fn generate_description_through_assistant(
     diff: &str,
     template: &str,
 ) -> Result<String, OpenAIError> {
-    get_assistant_response(assistant_id, diff, template).await
+    get_assistant_completion(assistant_id, diff, template).await
 }
