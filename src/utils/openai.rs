cc367dbbf3f55ee5db81e27ba6a5e72b3d7e934c
segersniels <segers.n@hotmail.com>
2024-02-08 15:19:10 +0100

    :sparkles: Add literal turbo preview model

--- src/utils/openai.rs
+++ src/utils/openai.rs
@@ -9,13 +9,14 @@ use async_openai::{
 };
 use tokio::time::sleep;
 
-pub const ALLOWED_MODELS: [&str; 6] = [
+pub const ALLOWED_MODELS: [&str; 7] = [
     "gpt-3.5-turbo",
     "gpt-3.5-turbo-16k",
     "gpt-3.5-turbo-1106",
     "gpt-4",
     "gpt-4-32k",
     "gpt-4-1106-preview",
+    "gpt-4-turbo-preview",
 ];
 
 const MAX_TOKEN_LENGTH: i32 = 512;
